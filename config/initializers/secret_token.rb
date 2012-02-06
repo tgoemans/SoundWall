e6a144c0d909d8bddb31bb5561b4a3f2c582b2ff
Thibault Goemans <goemansthibault@hotmail.com>
2012-02-06 14:13:39 +0100

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoundWall::Application.config.secret_token = '064b870f8440461d8070d55ba03d0338729970f317e39228fc98cdb3f7054f88a2b80fa5c1358fdf2a56e3ef27842486ca8cc4a8aa0b51e0d8a2307a5ce9bd92'
